1ef4f1d77fe193f3f392f8accfd25bafc5466b50
tbiesaart <tbiesaart@gmail.com>
2013-09-18 12:37:33 +0000

    Particle system is now 1 file

--- Pong/Pong/Pong/ParticleSystem.cs
+++ Pong/Pong/Pong/ParticleSystem.cs
@@ -8,8 +8,48 @@ using Microsoft.Xna.Framework.Graphics;
 namespace Pong
 {
     /// <summary>
-    /// Emitter of particles. When an instance of this class is made, it will automatically be drawn by the pariclemanager. (Of ParticleManager.draw is called)
+    /// Manager of particle emitters.
     /// </summary>
+    public static class ParticleManager
+    {
+        #region Fields
+        static List<Emitter> emitters = new List<Emitter>(); //All emitter managed by this one manager
+        #endregion
+
+        #region Methods
+        public static void update(GraphicsDevice g)
+        {
+            //Update all emitters
+            foreach (Emitter emitter in emitters)
+            {
+                emitter.update();
+            }
+        }
+        /// <summary>
+        /// Draw the full particle manager
+        /// </summary>
+        /// <param name="s">SpriteBatch. WARNING! DO NOT send in an open spriteBatch. Make sure it has been ended</param>
+        public static void draw(SpriteBatch s)
+        {
+            //Draw all emitters
+            foreach (Emitter emitter in emitters)
+            {
+                emitter.draw(s);
+            }
+        }
+        public static void addEmitter(Emitter emitter)
+        {
+            emitters.Add(emitter);
+        }
+        public static void removeEmitter(Emitter emitter)
+        {
+            emitters.Remove(emitter);
+        }
+        #endregion
+
+        #region Properties
+        #endregion
+    }
     public class Emitter
     {
         #region Fields
@@ -39,7 +79,7 @@ namespace Pong
             }
 
             //Loop through all particles. Kill if the particle isn't alive, otherwise update the particle.
-            for(int i = particles.Count() - 1; i >= 0; i--)
+            for (int i = particles.Count() - 1; i >= 0; i--)
             {
                 //Update all particles that are alive.
                 if (particles[i].isAlive)
@@ -90,7 +130,7 @@ namespace Pong
             for (int a; waitList >= 1; waitList--)
             {
                 //Spawn a particle
-                particles.Add(new Particle(new Vector2((float)MathHelper.Lerp(-spawnSpeed,spawnSpeed,(float)random.NextDouble()), (float)MathHelper.Lerp(-spawnSpeed,spawnSpeed,(float)random.NextDouble())), position,beginSize,endSize));
+                particles.Add(new Particle(new Vector2((float)MathHelper.Lerp(-spawnSpeed, spawnSpeed, (float)random.NextDouble()), (float)MathHelper.Lerp(-spawnSpeed, spawnSpeed, (float)random.NextDouble())), position, beginSize, endSize));
             }
         }
         #endregion
@@ -120,6 +160,57 @@ namespace Pong
 
         #region Properties
         public Vector2 Position { get { return position; } set { position = value; } }
+        #endregion
+    }
+    /// <summary>
+    /// Single particle managed by an emitter
+    /// </summary>
+    public class Particle
+    {
+        #region Fields
+        Vector2 position = Vector2.Zero;
+        float beginSize = 1f;
+        float endSize = 0f;
+        Vector2 speed = Vector2.Zero;
+        Texture2D texture = Assets.DummyTexture;
+        Color beginColor = Assets.Colors.ExplodingGreen; //Color on spawn
+        Color endColor = Assets.Colors.DimmGreen; //Color on death. (Fades from begin to end color)
+        float ttl = 30; //Amount of frames to live (60 = 1 sec)
+        float lifeTime = 0; //The age of the particle in frames
+        #endregion
+
+        #region Methods
+        public void update()
+        {
+            //Change Lifetime
+            lifeTime++;
+
+            //Move particle
+            position += speed;
+        }
+        public void draw(SpriteBatch s)
+        {
+            if (isAlive)
+            {
+                //Draw particle
+                float size = MathHelper.Lerp(beginSize, endSize, lifeTime / ttl);
+                s.Draw(texture, new Vector2(position.X - size / 2, position.Y - size / 2), null, Color.Lerp(beginColor, endColor, lifeTime / ttl), 0f, Vector2.Zero, size, SpriteEffects.None, 0);
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public Particle(Vector2 speed, Vector2 position, float beginSize, float endSize)
+        {
+            this.position = position;
+            this.beginSize = beginSize;
+            this.endSize = endSize;
+            this.speed = speed;
+        }
+        #endregion
+
+        #region Properties
+        public bool isAlive { get { if (ttl > lifeTime) { return true; } else { return false; } } }
         #endregion
     }
 }
